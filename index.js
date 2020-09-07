79b07171fdee7c3a5646957053a13106ea75e6a1
G G <georgiagray7@comcast.net>
2020-09-07 03:58:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,9 +48,9 @@ function decrement(n){
 }
 
 function makeInt (n){
-  parseInt('n', 10)
+  return parseInt('n', 10)
 }
 function preserveDecimal(n){
-  parseFloat('n')
+  return parseFloat('n')
 }
 
