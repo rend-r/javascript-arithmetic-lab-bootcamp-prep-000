89913a1d84b48aaf68d7272fb0b2b896f84367a3
G G <georgiagray7@comcast.net>
2020-09-07 03:04:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,6 @@ function multiply(a, b){
 function divide(a, b){
   return a / b
 }
-function increment(n){
-  return n++
-}
-function decrement(n){
-  return n--
-}
 
 var number = 10
 
@@ -25,4 +19,30 @@ function add5(){
 
 function divideBy3(){
   number /= 3
+}
+
+divideBy3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+number = 10
+
+add5()
+
+console.log(number)
+
+divideBy3()
+
+console.log(number)
+
+function increment(n){
+  return n ++
+}
+
+function decrement(n){
+  return n --
 }
